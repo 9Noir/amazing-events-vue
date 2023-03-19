f8a3ffb834eb3fc9b11b5319b5315daf565cd8ad
Noir <jat_9000@hotmail.com>
2023-03-18 23:26:38 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# AMAZING EVENTS
+This project is a website for showcasing events. It is built with HTML, CSS (using Tailwind), and JavaScript VUE.
+
+Live website https://9noir.github.io/amazing-events-vue/
+
+
+## Pages
+The website has several pages:
+
+- **Home**: displays all events in a summarized form, both past (colored with a gray filter) and upcoming. Events are shown with information such as category, image, date, description, price, and location. There are also filters by category and a search field that can be used to find specific events.
+- **Upcoming events**: shows the events that are yet to happen, with the same filters and search field as found on the home page.
+- **Past events**: similar to the upcoming events page, but only for past events.
+- **Contact**: a form for contacting and sending messages.
+- **Stats**: displays a table with statistics from past events, such as maximum and minimum attendance and the event with the highest capacity. There are also revenue statistics and percentage attendance grouped by category for both past and upcoming events.
+- **Details**: this page shows more detailed information about the event, in addition to the aforementioned data. It includes an estimate of event attendance and capacity.
+
+## External API
+All event data is obtained from an external API and loaded onto the page through a "fetch" request.
+
+## Responsive and Theme Selector
+The website is fully responsive and also includes a theme selector so that users can choose between a dark or light theme.
---
+++ assets/js/main.js
@@ -0,0 +1,92 @@
+const darkModeBtn = document.querySelector(".toggleDark-btn");
+const eventNotFound = [{ id: 0, image: "https://i.ibb.co/HzD6k2W/image.png", name: "No results found", date: new Date(), description: "Sorry, we couldn't find any matching events. Please try again with different search criteria.", category: "Event", place: "", capacity: "", assistance: "", price: "" }];
+const { createApp } = Vue;
+createApp({
+    data() {
+        return { currentDate: "", events: [], filteredEvents: [], categories: [], checkedCategories: [], searchText: "", contactContent: { showModal: false }, statsData: {} };
+    },
+    async created() {
+        setDarkMode();
+        const path = this.getParams();
+        if (path == "stats") return await this.getEventStats();
+        const data = await this.fetchData(path);
+        this.currentDate = data.currentDate;
+        this.events = data.events?.sort((event1, event2) => new Date(event2.date) - new Date(event1.date)) || [data.response];
+        this.categories = this.getUniqueCategories(this.events);
+    },
+    methods: {
+        fetchData(params) {
+            return fetch(`https://api-amazingevents.onrender.com/api/amazing-events${params || ""}`).then((response) => response.json());
+        },
+        getParams() {
+            const title = document.title.split(" ")[0].toLowerCase();
+            if (title.includes("past") || title.includes("upcoming")) return "?time=" + title;
+            if (title.includes("details")) return "/" + this.extractIdFromUrl();
+            if (title.includes("stats")) return "stats";
+            return "";
+        },
+        getUniqueCategories(events) {
+            return [...new Set(events.map((event) => event.category))].sort((a, b) => a.localeCompare(b));
+        },
+        splitDate(eventDate, dateIndex) {
+            return new Date(eventDate).toDateString().split(" ")[dateIndex];
+        },
+        extractIdFromUrl: () => new URLSearchParams(window.location.search).get("id"),
+        // selectEvent() {
+        //     this.contactContent.event = this.events.find((e) => e.id == this.extractIdFromUrl()).name;
+        // },
+        toggleContactModal(event) {
+            event?.preventDefault();
+            this.contactContent.showModal = !this.contactContent.showModal;
+        },
+        async getEventStats() {
+            const upcomingEvents = (await this.fetchData("?time=upcoming")).events;
+            const pastEvents = (await this.fetchData("?time=past")).events.sort((a, b) => a.assistance - b.assistance);
+            const [maxAssisEvent, minAssisEvent] = [pastEvents[pastEvents.length - 1], pastEvents[0]];
+            const maxCapEvent = pastEvents.sort((a, b) => b.capacity - a.capacity)[0];
+            let [upcomingCat, pastCat] = [this.getUniqueCategories(upcomingEvents), this.getUniqueCategories(pastEvents)];
+
+            pastCat = this.reduceToCategoryData(pastCat, pastEvents);
+            upcomingCat = this.reduceToCategoryData(upcomingCat, upcomingEvents);
+
+            this.statsData = { maxAssisEvent: maxAssisEvent, minAssisEvent: minAssisEvent, maxCapEvent: maxCapEvent, upcomingCat: upcomingCat, pastCat: pastCat };
+        },
+        reduceToCategoryData(categories, events) {
+            return categories
+                .map((currentCategory) =>
+                    events
+                        .filter((e) => e.category === currentCategory)
+                        .reduce(
+                            (accumulator, event) => {
+                                accumulator.revenues += event.price * (event.assistance || event.estimate);
+                                accumulator.attendance += event.assistance || event.estimate;
+                                accumulator.capacity += event.capacity;
+                                return accumulator;
+                            },
+                            { category: currentCategory, revenues: 0, attendance: 0, capacity: 0 }
+                        )
+                )
+                .sort((a, b) => b.revenues - a.revenues);
+        }
+    },
+    computed: {
+        filterEvents() {
+            this.filteredEvents = this.events.filter((event) => {
+                let categoryMatch = this.checkedCategories.includes(event.category) || this.checkedCategories.length === 0; //Filtrado de eventos para cada event, si su categoria esta incluida en checkedCategories o si no se capturo ningun checked
+                let searchMatch = JSON.stringify(event).toLowerCase().includes(this.searchText.toLowerCase());
+                return categoryMatch && searchMatch;
+            });
+        },
+    },
+}).mount("#app");
+
+function setDarkMode() {
+    document.documentElement.classList.toggle("dark", localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches));
+    darkModeBtn.classList.toggle("bi-moon-fill", document.documentElement.classList.contains("dark"));
+    darkModeBtn.classList.toggle("bi-moon", !document.documentElement.classList.contains("dark"));
+    document.body.style.display = "flex";
+}
+function toggleDarkMode() {
+    localStorage.theme = localStorage.theme === "dark" ? "light" : "dark";
+    setDarkMode();
+}
